f3416b7f3c967643ce2d0a40ba6360a59c9bd345
GAlmaguer68 <gustavo_almaguer@hotmsil.com>
2020-09-16 00:39:58 -0500

    Todo

---
+++ App1Test/App1Test/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1Test
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //Device.SetFlags(new string[] { "RadioButton_Experimental", "CarouselView_Experimental", "MediaElement_Experimental", "SwipeView_Experimental" });
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
